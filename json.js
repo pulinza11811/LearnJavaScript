3c68c497926bde6f2ed9da0010e28a04bed4c9e9
pulinza11811 <103770001+pulinza11811@users.noreply.github.com>
2024-02-13 10:39:58 +0700

    update json

--- json.js
+++ json.js
@@ -1,5 +1,14 @@
 let user =
 {
-    name:"Owen", age:21 , email:"pulindev@gmail.com"
+    name :"Owen",
+    age  : 21,
+    email:"pulindev@gmail.com",
+    color:"black",
+    size :"s",
+    phone : "0625470909",
+    Getusers:function()
+    {
+        return this.name+" =>"+this.email+" =>"+this.phone
+    }
 }
-console.log(user)
+console.log(user.name)
